1b0925eb87e426187ec8c8659d09e0e70e464d9b
Justin Mah <jmmxpdev@gmail.com>
2018-09-24 16:23:44 -0600

    Add emotion pictures, start list item layout

--- app/src/main/java/com/jmm/android/assignment1/controller/MainActivity.java
+++ app/src/main/java/com/jmm/android/assignment1/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jmm.android.assignment1;
+package com.jmm.android.assignment1.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jmm.android.assignment1.R;
 
 public class MainActivity extends AppCompatActivity {
 
